f4787ffd47d9bab17a89464b23755aa15b55383e
Krishanitd-dev <kmdona815@gmail.com>
2026-06-27 18:58:29 +1200

    Change app name in feature branch

--- task_api_app/config.py
+++ task_api_app/config.py
@@ -4,5 +4,5 @@ import os
 load_dotenv()
 
 DATABASE_NAME = os.getenv("DATABASE_NAME", "tasks.db")
-APP_NAME = os.getenv("APP_NAME", "My Task API")
+APP_NAME = os.getenv("APP_NAME", "My Task Management API")
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
